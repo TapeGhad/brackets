33189ba4342b46b18332694e996784c616a6b8d0
tapeghad <tapeghad@gmail.com>
2020-09-13 21:35:01 +0300

    feat: complete 20/20

--- src/index.js
+++ src/index.js
@@ -1,21 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
-  let arrOpen = ["(", "[", "{"];
-  let arrClose = [")", "]", "}"];
+  let arrOpen = ["(", "[", "{", "|", "7", "8", "1", "3", "5"];
+  let arrClose = [")", "]", "}", "|", "7", "8", "2", "4", "6"];
   console.log(str);
-  let finalOpen = false, countOpen = 0, countClose = 0, isOpen1 = false, isOpen2 = false, isOpen3 = false, bad = false;
-  let arr = str.split("").map((elem, index) => {
-    if (arrOpen.includes(elem)) {
-      if (elem === "(") isOpen1 = true;
-      if (elem === "[") isOpen2 = true;
-      if (elem === "{") isOpen3 = true;
-      countOpen ++;
+  let Open = "", storage = [], checkSame = true;
+  str.split("").map(elem => {
+    if (elem === "|" || elem === "7" || elem === "8") {
+      if (checkSame) {
+        storage.push(elem);
+        checkSame = false;
+      }
+      else {
+        Open = elem;
+        if (storage[storage.length - 1] === Open) storage.pop();
+        else storage.push(elem);
+      }
     }
-    else if (arrClose.includes(elem)) {
-      
-      countClose ++;
+    else {
+      if (arrOpen.includes(elem)) {
+        storage.push(elem);
+      }
+      if (arrClose.includes(elem)) {
+        Open = arrOpen[arrClose.indexOf(elem)];
+        if (storage[storage.length - 1] === Open) storage.pop();
+        else storage.push(elem);
+      }
     }
     return elem;
   });
-  if (arrOpen.includes(arr[arr.length - 1])) finalOpen = true;
-  return (finalOpen === false && countClose === countOpen) ? true : false;
+  return storage.length === 0;
 }
